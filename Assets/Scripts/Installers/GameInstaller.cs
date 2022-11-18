39dbd2fd0b0f5f61a4ae15fd1c2d28f381ee5b9c
Vladimir Dats <ovalnekvadrat@gmail.com>
2022-11-18 13:09:39 +0600

    Add plugins

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,35 @@
+﻿using ECS.DataSave;
+using ECS.Utils.Impls;
+using Game.Utils.MonoBehUtils;
+using Services.PauseService.Impls;
+using UnityEditor;
+using UnityEngine;
+using Zenject;
+using ZenjectUtil.Test.Extensions;
+
+namespace Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        [SerializeField] private GetPointFromScene getPointFromScene;
+        //[SerializeField] private PlayerInputModule playerInputModule;
+        //[SerializeField] private PrefabsBase prefabBase;
+        
+        public override void InstallBindings()
+        {
+            BindServices();
+        }
+
+        private void BindModules()
+        {
+            Container.Bind<GetPointFromScene>().FromInstance(getPointFromScene).AsSingle();
+        }
+        
+        
+        private void BindServices()
+        {
+            Container.BindInterfacesTo<SpawnService>().AsSingle();
+            Container.BindInterfacesTo<PauseService>().AsSingle();
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/ProjectPrefabsInstaller.cs
@@ -0,0 +1,19 @@
+﻿using DataBase.Objects;
+using DataBase.Objects.Impl;
+using UnityEngine;
+using Zenject;
+using ZenjectUtil.Test.Extensions;
+
+namespace Runtime.Installers
+{
+    [CreateAssetMenu(menuName = "Installers/ProjectPrefabsInstaller", fileName = "ProjectPrefabsInstaller")]
+    public class ProjectPrefabsInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private PrefabsBase prefabBase;
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<IPrefabsBase>().FromSubstitute(prefabBase).AsSingle();       
+        }     
+    }
+}
